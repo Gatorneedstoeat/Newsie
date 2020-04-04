e12f02f53ecefb9b50f2122c10bd01acb7028924
Samuel Slosser <samuel.slosser@gmail.com>
2020-04-04 11:30:29 -0400

    changed site name

--- src/sections/NavBar.js
+++ src/sections/NavBar.js
@@ -3,7 +3,7 @@ import {Navbar,Nav,Form,NavDropdown,FormControl,Button} from 'react-bootstrap'
 
 const NavBar = () => (
     <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="#home">Newsie</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
