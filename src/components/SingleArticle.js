b074518aad142bc05eb6ab19f2a49f4d6b022e38
Samuel Slosser <samuel.slosser@gmail.com>
2020-04-04 20:04:08 -0400

    added fonts added link removed default comments

--- src/components/SingleArticle.js
+++ src/components/SingleArticle.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import {Card} from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import ScrollAnimation from 'react-animate-on-scroll';
 
 const SingleArticle = ({ item }) => {
-    
-        return(
-    <ScrollAnimation animateIn="slideInUp" animateOnce={true}>
-        <Card>
-            <Card.Img variant="top" src={item.urlToImage} alt={item.title}/>
-            <Card.Body>
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Text>
-                    {item.description}
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    </ScrollAnimation>
-        )
+
+    return (
+        <ScrollAnimation animateIn="slideInUp" animateOnce={true}>
+            <a href={item.url} rel="noopener noreferrer">
+                <Card>
+                    <Card.Img variant="top" src={item.urlToImage} alt={item.title} />
+                    <Card.Body>
+                        <Card.Title>{item.title}</Card.Title>
+                        <Card.Text>
+                            {item.description}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </a>
+        </ScrollAnimation>
+    )
 
 }
 
