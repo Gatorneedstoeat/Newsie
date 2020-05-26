e643d28bf1e4a276b04f0b33e4d09df4d1f0906c
Sam Slosser <samuel.slosser@gmail.com>
2020-05-26 16:15:46 -0400

    Updates

---
+++ precache-manifest.8634a270529f13307ca632c73afea169.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692f6a81509a68c5ca3a72317082cd9e",
+    "url": "/Newsie/index.html"
+  },
+  {
+    "revision": "26f65e7c1933e64816d0",
+    "url": "/Newsie/static/css/2.f2ad0615.chunk.css"
+  },
+  {
+    "revision": "153413bcedfb6ef22fdf",
+    "url": "/Newsie/static/css/main.676011f2.chunk.css"
+  },
+  {
+    "revision": "26f65e7c1933e64816d0",
+    "url": "/Newsie/static/js/2.9cc87a4c.chunk.js"
+  },
+  {
+    "revision": "d72c695bb2fed22c8cbf125cbcb911ef",
+    "url": "/Newsie/static/js/2.9cc87a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153413bcedfb6ef22fdf",
+    "url": "/Newsie/static/js/main.d25fe22c.chunk.js"
+  },
+  {
+    "revision": "ec13abb055dd58145c6f",
+    "url": "/Newsie/static/js/runtime-main.3f3fffc0.js"
+  },
+  {
+    "revision": "14aaff013398c35430cc935d1e4dcd99",
+    "url": "/Newsie/static/media/BreeSerif-Regular.14aaff01.ttf"
+  },
+  {
+    "revision": "7059acee3b9d8ea9744c26ab45135a8b",
+    "url": "/Newsie/static/media/SpecialElite-Regular.7059acee.ttf"
+  },
+  {
+    "revision": "15db01c5265adb04fd5e691e5ef26009",
+    "url": "/Newsie/static/media/ZillaSlab-Regular.15db01c5.ttf"
+  }
+]);
